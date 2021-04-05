3bc59a760e2b5bbd26ab7c450d7897a568d4b83a
pawelsa <pawel.sapek97@gmail.com>
2021-04-05 13:01:09 +0200

    added simple theming

--- movie_app_flutter/lib/main.dart
+++ movie_app_flutter/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:movie_app_flutter/resources/colors.dart';
+
+const fontItcAvantGardeStd = 'itc_avant_garde_std';
 
 void main() {
   runApp(ProviderScope(child: MyApp()));
@@ -9,12 +12,63 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
     return MaterialApp(
       title: 'Flutter Demo',
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        textTheme: textTheme!.copyWith(
+          headline1: textTheme.headline1!.copyWith(
+            fontFamily: fontItcAvantGardeStd,
+            fontWeight: FontWeight.w600,
+            color: MovieColors.greyText,
+          ),
+          headline2: textTheme.headline2!.copyWith(
+            fontFamily: fontItcAvantGardeStd,
+            fontWeight: FontWeight.w700,
+            color: Colors.black,
+          ),
+          headline3: textTheme.headline3!.copyWith(
+            fontFamily: fontItcAvantGardeStd,
+            color: MovieColors.greyText,
+          ),
+          headline4: textTheme.headline4!.copyWith(
+            fontFamily: fontItcAvantGardeStd,
+            fontWeight: FontWeight.w600,
+            color: Colors.black,
+          ),
+          headline5: textTheme.headline5!.copyWith(
+            fontFamily: fontItcAvantGardeStd,
+            fontWeight: FontWeight.w400,
+            color: MovieColors.greyText,
+          ),
+          headline6: textTheme.headline6!.copyWith(
+            fontFamily: fontItcAvantGardeStd,
+          ),
+          subtitle1: textTheme.subtitle1!.copyWith(
+            fontFamily: fontItcAvantGardeStd,
+          ),
+          bodyText1: textTheme.bodyText1!.copyWith(
+            fontFamily: fontItcAvantGardeStd,
+          ),
+          bodyText2: textTheme.bodyText2!.copyWith(
+            fontFamily: fontItcAvantGardeStd,
+          ),
+          button: textTheme.button!.copyWith(
+            fontFamily: fontItcAvantGardeStd,
+            color: Colors.white,
+          ),
+          caption: textTheme.caption!.copyWith(
+            fontFamily: fontItcAvantGardeStd,
+            color: MovieColors.greyText,
+            fontWeight: FontWeight.w500,
+          ),
+          overline: textTheme.overline!.copyWith(
+            fontFamily: fontItcAvantGardeStd,
+          ),
+        ),
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
---
+++ movie_app_flutter/lib/resources/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MovieColors {
+  static const greyPill = const Color(0xFFACADAD);
+  static const greyButtonText = const Color(0xFF9B9B9B);
+  static const yellow = const Color(0xFFFFBC06);
+  static const nonSelectedText = const Color(0xFFA9AAAA);
+  static const greyText = const Color(0xFFACADAD);
+  static const backgroundStart = Colors.white;
+  static const backgroundEnd = const Color(0xFFE7E7E7);
+  static const greyButton = const Color(0xFFEAEAEA);
+// static const pageGradient = Brush.linearGradient(
+//     colors = listOf(backgroundStart, backgroundEnd)
+// )
+}
